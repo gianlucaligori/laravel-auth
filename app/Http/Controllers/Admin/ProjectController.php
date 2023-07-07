3fbb311c9d132194f07ebf4713d2550aeb0b4868
gianlucaligori <gianluca.ligori.95@gmail.com>
2023-07-07 15:32:48 +0200

    add final

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -19,4 +19,50 @@ class ProjectController extends Controller
     {
         return view('admin.projects.show', compact('project'));
     }
+    public function create()
+    {
+        return view('admin.projects.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $newProject = new Project();
+
+        $newProject->title          = $data['title'];
+        $newProject->date           = $data['date'];
+        $newProject->name           = $data['name'];
+        $newProject->surname        = $data['surname'];
+        $newProject->description    = $data['description'];
+        $newProject->save();
+
+        return to_route('admin.projects.show', ['project' => $newProject]);
+    }
+
+    public function edit(Project $project)
+    {
+        return view('admin.projects.edit', compact('project'));
+    }
+
+    public function update(Request $request, Project $project)
+    {
+        $data = $request->all();
+
+        $project->title         = $data['title'];
+        $project->date          = $data['date'];
+        $project->name          = $data['name'];
+        $project->surname       = $data['surname'];
+        $project->description   = $data['description'];
+        $project->update();
+
+        return to_route('admin.projects.show', ['project' => $project]);
+    }
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+
+        return to_route('admin.projects.index')->with('delete_success', $project);
+    }
 }
--- resources/views/Admin/projects/show.blade.php
+++ resources/views/Admin/projects/show.blade.php
@@ -4,22 +4,21 @@
     <h1 class="text-center">Project</h1>
     <div class="container">
         <div class="row row-cols-3">
-            @foreach ($projects as $project)
-                <div class="col">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $project->title }}</h5>
-                        <p class="card-text">{{ $project->description }}</p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">{{ $project->date }}</li>
-                        <li class="list-group-item">{{ $project->name }}</li>
-                        <li class="list-group-item">{{ $project->surname }}</li>
-                    </ul>
-                    <div class="card-body">
-                        <a class="btn btn-primary" href="{{ route('projects.show', ['project' => $project->id]) }}">VIEW</a>
-                    </div>
+            <div class="col">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $project->title }}</h5>
+                    <p class="card-text">{{ $project->description }}</p>
                 </div>
-            @endforeach
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{{ $project->date }}</li>
+                    <li class="list-group-item">{{ $project->name }}</li>
+                    <li class="list-group-item">{{ $project->surname }}</li>
+                </ul>
+                <div class="card-body">
+                    <a class="btn btn-primary"
+                        href="{{ route('admin.projects.show', ['project' => $project->id]) }}">VIEW</a>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
